981be0dc22cad988b95c3769a897c2b000cc0617
sumutoni <steumutoni@gmail.com>
2023-05-26 06:40:49 -0700

    added /api/v1/stats route to count each object type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Index """
-from api.v1.views import app_views
+from api.v1.views import iapp_views, storage
 from flask import Flask, jsonify, Blueprint
 
 
@@ -9,3 +9,14 @@ def status():
     """ Status of API """
     stat = {"status": "OK"}
     return jsonify(stat)
+
+
+@app_views.route('/api/v1/stats', strict_slashes=False)
+def stats():
+    """retrieves the number of each objects by type"""
+    models = {"User": "users", "Amenity": "amenities", "Place": "places",
+              "Review": "reviews", "City": "cities", "State": "states"}
+    stats = {}
+    for k in models.keys():
+        stats[models[k]] = storage.count(k)
+    return jsonify(stats)
