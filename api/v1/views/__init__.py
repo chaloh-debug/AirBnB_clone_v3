bf352030b1ec2fadae5c0ac4b739a4f97522a083
chaloh-debug <charlesgitahi.cn@gmail.com>
2023-05-26 21:22:53 +0300

    init update

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 """ Init """
 from flask import Blueprint
+from models import storage
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models.city import City
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 from api.v1.views.index import *
