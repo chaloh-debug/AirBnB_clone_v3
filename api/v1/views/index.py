18bb49982ec792ff22e8eacd13a6f64103c0a303
chaloh-debug <charlesgitahi.cn@gmail.com>
2023-05-26 06:33:04 +0300

    api update

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,7 +4,7 @@ from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
+@app_views.route('/status', methods=['GET'])
 def status():
     """ Status of API """
     stat = {"status": "OK"}
