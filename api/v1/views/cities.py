c999de0a117cc51b25e35ea8a1376b296d1eda19
chaloh-debug <charlesgitahi.cn@gmail.com>
2023-05-27 03:13:07 +0300

    cities fix

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -6,7 +6,9 @@ from models.city import City
 from api.v1.views import app_views
 from flask import Flask, abort, make_response, jsonify, request
 
-@app_views.route('/states/<state_id>/cities', methods=['GET'], strict_slashes=False)
+
+@app_views.route('/states/<state_id>/cities',
+                 methods=['GET'], strict_slashes=False)
 def get_cities(state_id):
     """retrieve all city objects """
     all_cities = []
@@ -17,6 +19,7 @@ def get_cities(state_id):
         all_cities.append(cts.to_dict())
     return jsonify(all_cities)
 
+
 @app_views.route('/cities/<city_id>', methods=['GET'], strict_slashes=False)
 def get_city(city_id):
     """ retrieve a city based on city_id """
@@ -25,7 +28,8 @@ def get_city(city_id):
         abort(404)
     return jsonify(citi.to_dict())
 
-@app_views.route('/cities/<city_id>', methods=['GET'], strict_slashes=False)
+
+@app_views.route('/cities/<city_id>', methods=['DELETE'], strict_slashes=False)
 def delete_city(city_id):
     """ delete a city based on city_id """
     citi = storage.get(City, city_id)
@@ -35,7 +39,9 @@ def delete_city(city_id):
     storage.save()
     return make_response(jsonify({}), 200)
 
-@app_views.route('/states/<state_id>/cities', methods=['POST'], strict_slashes=False)
+
+@app_views.route('/states/<state_id>/cities', methods=['POST'],
+                 strict_slashes=False)
 def create_city(state_id):
     """creates a city object """
     states = storage.get(State, state_id)
@@ -52,9 +58,10 @@ def create_city(state_id):
     storage.save()
     return make_response(jsonify(citi.to_dict()), 201)
 
+
 @app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
 def put_city(city_id):
-    """ retrieve a city based on city_id """
+    """ update a city based on city_id """
     citi = storage.get(City, city_id)
     if not citi:
         abort(404)
@@ -68,5 +75,5 @@ def put_city(city_id):
         if k not in ignore_keys:
             setattr(citi, k, v)
 
-    storage.save()
+    citi.save()
     return make_response(jsonify(citi.to_dict()), 200)
